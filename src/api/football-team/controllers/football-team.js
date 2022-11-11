3df146b57682b4906f01a537ff3d98f57ec17e90
huylesitdn <mrsjiodev@gmail.com>
2022-11-11 14:03:55 +0700

    update

---
+++ src/api/football-team/controllers/football-team.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * football-team controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::football-team.football-team');
---
+++ src/api/football-team/routes/football-team.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * football-team router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::football-team.football-team');
