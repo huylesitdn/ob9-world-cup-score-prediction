897bc10e487d7dec6a9da423231fc77a200fc9c1
huylesitdn <mrsjiodev@gmail.com>
2022-11-11 12:17:53 +0700

    change sqlite

--- config/database.js
+++ config/database.js
@@ -11,22 +11,36 @@
 //     },
 //   },
 // });
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+
+
+
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: config.host,
+//       port: config.port,
+//       database: config.database,
+//       user: config.user,
+//       password: config.password,
+//       ssl: {
+//         rejectUnauthorized: false
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
