71e2080a8434b33bf2f2774a99ccbf0c53c9cf79
huylesitdn <mrsjiodev@gmail.com>
2023-05-09 23:23:11 +0700

    update db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'worldcup'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '12345'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'worldcup'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', '12345'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
 
 
 
@@ -35,14 +35,14 @@ module.exports = ({ env }) => ({
 // });
 
 
-// const path = require('path');
+const path = require('path');
 
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
